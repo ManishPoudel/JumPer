c778491775181a240e33316ed07601f61cc1ceb0
ManishPoudel <poudelmanish321@gmail.com>
2022-02-10 20:24:58 +0545

    it jumps now

--- jumper.c
+++ jumper.c
@@ -10,6 +10,10 @@ SDL_Window *window;
 SDL_Renderer *renderer;
 SDL_Surface *surface;
 SDL_Texture *texture;
+SDL_Rect srcImg;
+SDL_Rect standImg;
+SDL_Rect jumpImg;
+
 
 int track;
 
@@ -27,6 +31,9 @@ int main(){
 }
 
 void gameplay(void){
+    int speed = 10;
+    int distance = 0;
+
     surface = IMG_Load("gfx/jumper.png");
     if(!surface){
         printf("no surface created");
@@ -38,25 +45,82 @@ void gameplay(void){
         printf("no texture created\n");
         quitAll();
     }
-    SDL_Rect srcImg;
+
     SDL_QueryTexture(texture, NULL, NULL, &srcImg.w, &srcImg.h);
     int closeReq = 0;
 
+    srcImg.h /= 3;
+    srcImg.w /= 4.5;
+    standImg.h = 64;
+    standImg.w = 54;
+    standImg.x = 0;
+    standImg.y = 0;
+
+    int xpos = (WINDOW_WIDTH - srcImg.w);
+    int ypos = (WINDOW_HEIGHT - srcImg.h);
+    srcImg.x = xpos;
+    srcImg.y = ypos;
+    int up = 0, down = 0, left = 0, right = 0;
     while(!closeReq){
-
-       /*  SDL_Event event;
-
+        SDL_Event event;
         while(SDL_PollEvent(&event)){
-            if(event.type == SDL_QUIT){
+            switch(event.type){
+            case SDL_QUIT:
                 closeReq = 1;
+                break;
+            case SDL_KEYDOWN:
+                switch(event.key.keysym.scancode){
+                case SDL_SCANCODE_UP:
+                    up = 1;
+                    break;
+                case SDL_SCANCODE_DOWN:
+                    down = 1;
+                    break;
+                case SDL_SCANCODE_LEFT:
+                    left = 1;
+                    break;
+                case SDL_SCANCODE_RIGHT:
+                    right = 1;
+                    break;
+                }
+                break;
+            /* case SDL_KEYUP:
+                switch(event.key.keysym.scancode){
+                case SDL_SCANCODE_UP:
+                    up = 0;
+                    break;
+                case SDL_SCANCODE_DOWN:
+                    down = 0;
+                    break;
+                case SDL_SCANCODE_LEFT:
+                    left = 0;
+                    break;
+                case SDL_SCANCODE_RIGHT:
+                    right = 0;
+                    break;
+                }
+            break; */
             }
         }
- */
+        if(up == 1 && down == 0 && distance < 70){
+            srcImg.y -= speed;
+            distance += 10;
+        } else if(up == 1 && distance > 0){
+            srcImg.y += speed;
+            distance -= 10;
+            down = 1;
+        } else{
+            up = 0;
+            down = 0;
+        }
+        if(left == 1){
+            srcImg.x -= speed;
+            left = 0;
+        }
         SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, texture, NULL, &srcImg);
+        SDL_RenderCopy(renderer, texture, &standImg, &srcImg);
         SDL_RenderPresent(renderer);
-        SDL_Delay(1000);
-        closeReq = 1;
+        SDL_Delay(1000 / 70);
     }
     return;
 }
